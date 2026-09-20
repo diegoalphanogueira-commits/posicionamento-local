0060e940e59465cb9550cb5fe6f34175429e185a
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-20 11:21:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ document.addEventListener("DOMContentLoaded", function () {
         ========================= */
 
         var radarUrl =
-            "https://radar.metodoflow.co.br/";
+            "https://radar.metodoflow.com.br/";
 
 
         var params =
